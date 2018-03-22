7ad47c2ec4f4bca0bfdd60611d44be920c2c91c9
Nacer A <shindelys@gmail.com>
2018-03-22 14:03:21 +0100

    Ajout du code de redirection vers la page d'accueil dans le controleur principal

--- ToolsRusWeb/src/webapp/Controleur.java
+++ ToolsRusWeb/src/webapp/Controleur.java
@@ -2,6 +2,7 @@ package webapp;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,15 +24,20 @@ public class Controleur extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 
-
+	private RequestDispatcher 	disp;
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		String path = request.getPathInfo();
+		if (path == null || path.equals("/")) accueil(request, response);
+		
 		
 	}
 
-
+	private void accueil(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		disp = request.getRequestDispatcher("/index.jsp");
+		disp.forward(request,response);	
+	}
 	
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
